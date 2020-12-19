ddb6a88c36ec37f925a9fb515f23f6b7adcb39b4
Mark Rieck <markrieck81@gmail.com>
2020-12-18 19:51:02 -0600

    Gibberish detector seems buggy, owl is blocking some task rewards he shouldn't.  Remove for now

--- js/options/task_list.js
+++ js/options/task_list.js
@@ -350,7 +350,7 @@ TASKS.buttonHandlers = function(){
         console.log("gibberish score");
         console.log(testGibberish);
         
-        if(diffSec > 180 && testGibberish < 67){
+        if(diffSec > 180){
             chrome.runtime.sendMessage({method: "addVacationTime", time: theMin, task_text: taskText, subtask: 'no', subtask_count: taskObject.subtasks.length},
             function(response)
             {
@@ -433,7 +433,7 @@ TASKS.buttonHandlers = function(){
         console.log("gibberish score");
         console.log(testGibberish);        
       
-        if(diffSec > 180 && testGibberish < 50){
+        if(diffSec > 180){
             chrome.runtime.sendMessage({method: "addVacationTime", time: theMin, task_text: taskText, subtask: 'yes'},
             function(response)
             {
